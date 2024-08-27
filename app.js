20f199e07b355170021d3991b58a2930fc3808d5
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-27 17:37:48 +0500

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var fiName = document.getElementById("firstName")
+
+// fiName.addEventListener(onclick ,function (n){
+//     console.log(fiName)
+// })
+
+
+function firstName(fiName){
+    console.log("firstName")
+}
+
