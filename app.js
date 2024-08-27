202024cfc154509a38f92a2a21adb34fab25722e
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-27 19:01:58 +0500

    commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var fiName = document.getElementById(firstName)
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-// fiName.addEventListener(onclick ,function (n){
-//     console.log(fiName)
-// })
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var fathername = document.getElementById('fathername').value;
+    var DOB = document.getElementById('DOB').value;
+    var age = document.getElementById('age').value;
 
-
-function firstName(fiName){
-    console.log(firstName.value)
-}
-
-// document.console.log()
+    console.log('Name:', name);
+    console.log('Father Name:', fathername);
+    console.log('Date Of Birth:', DOB);
+    console.log('Email:', email);
+    console.log('Age:', age);
+});
