15cb7e14e685e1285670f8cc8f4e487144c072ab
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-28 13:44:22 +0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('myForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var fathername = document.getElementById('fathername').value;
+//     var dob = document.getElementById('dob').value;
+//     var age = document.getElementById('age').value;
+
+//     console.log('Name:', name);
+//     console.log('Father Name:', fathername);
+//     console.log('Date Of Birth:', dob);
+//     console.log('Email:', email);
+//     console.log('Age:', age);
+// });
+
+
+document.getElementById('myForm').addEventListener('submit',function(event){
+
+    
 
     var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
     var fathername = document.getElementById('fathername').value;
-    var DOB = document.getElementById('DOB').value;
+    var email = document.getElementById('email').value;
+    var dob = document.getElementById('dob').value;
     var age = document.getElementById('age').value;
 
-    console.log('Name:', name);
-    console.log('Father Name:', fathername);
-    console.log('Date Of Birth:', DOB);
-    console.log('Email:', email);
-    console.log('Age:', age);
-});
+    var userdata ={
+        Name: name,
+        FatherName: fathername,
+        Email: email,
+        DOB: dob,
+        Age: age,
+        
+    }
+
+    event.preventDefault();
+    
+    console.log(userdata)
+})
